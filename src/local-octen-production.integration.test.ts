4fc63220c5df6c3c0a0114152f626e3868c4961c
Will Hampson <whamp@ggl.slmail.me>
2026-08-11 17:52:58 -0700

    test: isolate local inference thread behavior

--- src/local-octen-production.integration.test.ts
+++ src/local-octen-production.integration.test.ts
@@ -114,7 +114,7 @@ void test(
         embeddingProfile: RecallEmbeddingProfile.LOCAL_OCTEN,
         localModelRootDirectory: modelRootDirectory,
         localEmbeddingParallelism: 2,
-        localEmbeddingIntraOperationThreads: 2,
+        localEmbeddingIntraOperationThreads: 1,
       })}\n`,
       'utf8',
     );
